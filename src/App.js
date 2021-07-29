778870650ef225f6975d65a67c8458f5c0aa24ef
Thomas Kalnik <kalnik.thomas@gmail.com>
2021-07-29 00:07:36 -0500

    analysis page

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
     <Route path="/smallcaps" component={Smallcaps}/>
     <Route path="/analysis" component={Analysis}/>
     <Route path="/about" component={About}/>
-    <Redirect from="/" to="/analysis"/>
+    <Redirect from="/" to="/main"/>
     </div>
     </ThemeProvider>
     </div>
